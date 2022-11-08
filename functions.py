3ac254fff202e49461becd1bafc904f3bddfd727
Stockman-Jr <stockmanmimmi@gmail.com>
2022-11-08 09:18:55 +0000

    Add function to prompt user and validate input

--- functions.py
+++ functions.py
@@ -7,3 +7,13 @@ def typing(text):
         sys.stdout.write(character)
         sys.stdout.flush()
         time.sleep(0.02)
+
+
+def input_checker(prompt, opts):
+    choice = input(prompt)
+
+    while choice not in opts:
+        print(f"Please enter a valid choice {opts}")
+        choice = input(prompt)
+
+    return choice
