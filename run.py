a05e0f5606eb94ca5be8e8820771a3f56b26121f
Stockman-Jr <stockmanmimmi@gmail.com>
2022-11-08 08:46:15 +0000

    Add functions needed and docstring descriptions

--- run.py
+++ run.py
@@ -8,6 +8,10 @@ OPTIONS = ['a', 'b', 'c', 'd', 'east', 'south', 'west']
 
 
 def start_game():
+    """
+    Prompts user for a name, gives a short introduction
+    Then provides to option to start the game
+    """
     typing("Well, hello stranger.\n")
     player_name = input("Tell me your name.. >\n")
     typing(f"I see... {player_name}. Welcome.\n")
@@ -17,3 +21,52 @@ def start_game():
     # time.sleep(1)
     typing(f"Good luck {player_name}....\n")
     typing(input("Press enter to start game...\n"))
+
+
+def intro_scene():
+    """
+    First scene of the game, where user will be able
+    to choose which direction to take
+    """
+
+
+def fruit_grove():
+    """
+    If user chose east from intro scene, this function will play out.
+    Some story will be told and then user will be provided with two options.
+    """
+
+
+def three_fork_path():
+    """
+    If user chose south from intro_scene, this function will play out.
+    Some story will be told and then user will be prompted to choose
+    from three directions.
+    """
+
+
+def flashlight_scene():
+    """
+    If user chose west from three_fork_path, this function will play out.
+    The user fill find a flashlight and choose whether to keep it or not. 
+    That choice will update the global modern_flashlight variable.
+    A scene will then play out and promp user to choose from two options.
+    """
+
+
+def creatures_den():
+    """
+    If user chose east from three_fork_path, this function will play out.
+    This is where the creature lives, and user will only survive if 
+    old_flashlight is True.
+    """
+
+
+def abandoned_house():
+    """
+    If user chose south from three_fork_path, this function will play out.
+    User can only reach this point if modern_flashlight is True
+    """
+
+
+start_game()
