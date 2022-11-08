e9c2c084c377ec3fbf7e9f7e5353a3d6a9902db2
Stockman-Jr <stockmanmimmi@gmail.com>
2022-11-08 08:12:11 +0000

    Add the long story parts to story.py

---
+++ story.py
@@ -0,0 +1,67 @@
+INTRO_MSG = ("""
+            Your story begins with you waking up in the middle of a forest, surrounded by the dark of the night with no memory of how you got there.
+          
+            All you do know, as reality starts to sink in, is that you have to get out of there... alive.
+           
+            Make the right choices, and you just might succeed.
+            """)
+
+DEAD_GROVE = ("""
+            You walk towards the grove and stop at the entrace to look around, the fruits looks delicious.
+          
+            As you start walking to pick up some fruit for your journey, you suddenly feel the ground beneath you give in..
+          
+            "AAAaaahh"....
+          
+            You wake up to find yourself  at the bottom of a pitfall. 
+            You look up and see a pair of glowing eyes with a huge grin looking down at you...
+            """)
+
+FOUND_ITEM = ("""
+            You arrive at the source of the light you see a small leaf pile on the left side of the trail where the light is emanating from.
+          
+            You dig through the leafpile and find a flashlight! A strong one too, how strangely convenient!
+            """)
+
+ENCOUNTER = ("""
+            As you're crouched on the trail, proudly inspecting your newfound treasure, the hairs on your neck suddenly starts to rise..
+
+            You're being watched. 
+
+            Some strange distorted noises emits from the direction you were walking towards.
+            """)
+
+HOUSE = ("""
+           You start walking, continuing south off the path. 
+
+           After a while you arrive at a small, abandoned house.  
+           You might find something useful in there, you go inside...
+     
+           Dusty with lots of spiderwebs, how cozy. Looks like no one has set foot in there for some time.
+          
+           Handwritten notes and books are scattered everywhere, someone's been busy.
+
+           You wonder what whomever was here was writing about, but it seems to be written in some language unkown to you.
+         
+           You look around...
+            """)
+
+HOUSE_ITEM = ("""
+           You direct your flashlight towards the floor underneath the desk, and you notice something silver sticking out from a pile of notes.
+           You crouch down and pick it up.
+          
+            It looks like a really old flashlight in the shape of a square with a handle, you've never seen anything like it.
+          
+           There's some kind of switch on the back, you turn it and incredibly enough, it works!
+           """)
+
+HOUSE_ENCOUNTER = ("""
+           You suddenly hear twigs cracking outside the broken window. 
+           Something is outside.
+
+           You take a deep breath and look toward the broken window. 
+           You see the shape of a distorted lower body, violently muscular and covered in rippling shadows.
+
+           You are frozen in horror, trying to decide what to do, when a huge clawed hand splinters the window frame.
+           Looks like it's about to crouch and take a look inside.
+           """)
