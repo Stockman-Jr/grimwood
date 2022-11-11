a36e90f9d4c1b846566b095cd8517649e864d9d6
Stockman-Jr <stockmanmimmi@gmail.com>
2022-11-11 07:54:18 +0000

    Fix whitespace to make it more even and clean

--- run.py
+++ run.py
@@ -40,19 +40,19 @@ def intro_scene():
     """
     global visited
     if visited is False:
-        typing(
-         """It's almost pitch black, you give yourself a minute to adjust your
-            eyes to the darkness.\n""")
-        typing(
-            """You stand up and look around, you're standing on a path that
-               splits in two directions.\n""")
+        typing("""
+        It's almost pitch black, you give yourself a minute to adjust your
+        eyes to the darkness.\n""")
+        typing("""
+        You stand up and look around, you're standing on a path that
+        splits in two directions.\n""")
     else:
         typing(
             "You're back on the path that splits in two directions.\n")
     time.sleep(1)
 
     choices = input_checker(
-        "Which way will you go? Type east or south.\n", OPTIONS[4:6])
+        "Which way will you go? Type east or south.\n\n", OPTIONS[4:6])
 
     if choices == "east" and visited is False:
         fruit_grove()
@@ -71,28 +71,26 @@ def fruit_grove():
     """
     global visited
 
-    typing(
-        "You've been walking for ten minutes when you suddenly stop.\n ")
-    typing(
-        """You see in front of you what looks like a small grove
-        surrounded by trees bearing fruit. 
-        You stop to think..\n""")
+    typing("You've been walking for ten minutes when you suddenly stop.\n")
+    typing("""
+    You see in front of you what looks like a small grove
+    surrounded by trees bearing fruit.
+    You stop to think..\n""")
 
-    typing(
-        """
-        A. Some fruit might come in handy for your survival, after all you
-        have no idea where you are and how long you might be 
-        stuck in these woods. Go forward and pick up some fruit.\n""")
-    typing(
-       """B. Seems like a waste of time, you're not hungry. 
-       Go back and head south instead.\n""")
+    typing("""
+    A. Some fruit might come in handy for your survival, after all you
+    have no idea where you are and how long you might be
+    stuck in these woods. Go forward and pick up some fruit.\n""")
+    typing("""
+    B. Seems like a waste of time, you're not hungry.
+    Go back and head south instead.\n\n""")
 
     choices = input_checker(
         "What will you do?(a/b) >>", OPTIONS[0:2])
 
     if choices == "a":
         # typing(story.DEAD_GROVE)
-        game_over("You died.. Didn't last very long did you?\n", start_game)
+        game_over("Didn't last very long did you?\n", start_game)
     elif choices == "b":
         visited = True
         intro_scene()
@@ -106,16 +104,15 @@ def three_fork_path():
     from three directions.
     """
 
-    typing("""You arrive at a three-way-fork in the path.\n
-
-              You stop to think...""")
+    typing("""
+    You arrive at a three-way-fork in the path.\n
+    You stop to think...\n""")
+    typing("Looking east, you see a strange, faint light.\n")
+    typing("""
+    Looking west, the forest looks denser,
+    making it darker and harder to see.\n""")
     typing(
-        "Looking east, you see a strange, faint light.\n")
-    typing(
-        """Looking west, the forest looks denser,
-          making it darker and harder to see.\n""")
-    typing(
-        "Maybe you should ignore the paths and continue south, off piste.\n")
+        "Maybe you should ignore the paths and continue south, off piste.\n\n")
 
     choices = input_checker(
         "Where will you go?(west/east/south) >> ", OPTIONS[4:8])
@@ -131,14 +128,13 @@ def three_fork_path():
         As you walk for hours, struggling to see
         in the darkness, you start to realise that you're lost.
         And as you entered a denser, almost pitch-black part of the woods,
-        you didn't see that you were
-        walking towards a cliff..\n""", start_game)
+        you could'nt see the cliff you were walking towards..\n""", start_game)
 
 
 def flashlight_scene():
     """
     If user chose west from three_fork_path, this function will play out.
-    The user fill find a flashlight and choose whether to keep it or not. 
+    The user fill find a flashlight and choose whether to keep it or not.
     That choice will update the global modern_flashlight variable.
     A scene will then play out and promp user to choose from two options.
     """
@@ -154,7 +150,8 @@ def flashlight_scene():
         typing(story.ENCOUNTER)
     elif userInput == "no":
         modern_flashlight = False
-        win_game("""For some reason, unbeknownst to anyone,
+        win_game("""
+        For some reason, unbeknownst to anyone,
         you decided to not keep the flashlight and kicked
         it far off into the woods.
 
@@ -164,7 +161,8 @@ def flashlight_scene():
 
     typing("You take a second to think...\n")
 
-    typing("""A. It's probably your imagination, or some kind of animal. 
+    typing("""
+    A. It's probably your imagination, or some kind of animal.
     Direct your flashlight towards the sounds and see what it is.\n""")
     typing("B. Trust your instincs, flee! Run back to the three-way-fork.\n")
 
@@ -172,7 +170,8 @@ def flashlight_scene():
         "What will you do?(a/b) >> ", OPTIONS[0:2])
 
     if choices == "a":
-        game_over("""Whatever it was, it pinpointed your location
+        game_over("""
+        Whatever it was, it pinpointed your location
         once you shone your light towards it, and it was fast.
         """, start_game)
     elif choices == "b":
@@ -193,8 +192,9 @@ def creatures_den():
         game_over("then it charges.", start_game)
     elif old_flashlight is True:
         typing(story.DEN_SURVIVAL)
-        win_game("""As you reach the end of the tunnel
-         you see city lights flickering in the distance..""", start_game)
+        win_game("""
+        As you reach the end of the tunnel
+        you see city lights flickering in the distance..""", start_game)
     else:
         game_over("""
         You walk into pitch-black darkness, you can't see a thing.
@@ -215,26 +215,28 @@ def abandoned_house_encounter():
     time.sleep(1)
     choices = input_checker("""
     What will you do?(a/b/c)
-                            
+
     A. Slap yourself to release you from the paralyzing fear, and RUN!!!
 
-    B. You're frozen in fear, you stay still and watch as you squeeze the old 
+    B. You're frozen in fear, you stay still and watch as you squeeze the old
     flashlight.
 
     C. Turn off the flashlight, hold your breath and hide under the desk.
-    
+
     >>> """, OPTIONS[0:3])
     if choices == "a":
-        game_over("""You tried to run, but trying to outrun this 
-        creature proved to be fatal... You died.""", start_game)
+        game_over("""
+        You tried to run, but trying to outrun this
+        creature proved to be fatal...""", start_game)
     elif choices == "b":
         typing(story.HOUSE_ENCOUNTER_OUTCOME)
         # TODO
     elif choices == "c":
-        game_over("""You close your eyes and cover your mouth,
+        game_over("""
+        You close your eyes and cover your mouth,
         hoping for the nightmare to be over..
         And so your hopes came true, but not in a good way..
-        You died.""", start_game)
+        """, start_game)
 
 
 def abandoned_house():
@@ -245,12 +247,15 @@ def abandoned_house():
     global old_flashlight
 
     typing(story.HOUSE)
-    typing("""In the left corner there's a bed with a nightstand next to it,
-              seems to have two drawers.\n""")
-    typing("""In front of you there is a desk and a chair, with piles of notes 
-              and books scattered across the floor and desk.\n""")
-    typing("""You should probably look around..
-              You think about where to start...\n""")
+    typing("""
+        In the left corner there's a bed with a nightstand next to it,
+        seems to have two drawers.\n""")
+    typing("""
+        In front of you there is a desk and a chair, with piles of notes
+        and books scattered across the floor and desk.\n""")
+    typing("""
+        You should probably look around..
+        You think about where to start...\n""")
 
     print("""
           A. Underneath the bed
@@ -263,9 +268,9 @@ def abandoned_house():
 
           """)
     for choices in range(2):
-        choices = input_checker(
-            """What would you like to investigate?(a/b/c/d) >>
-            """, OPTIONS[0:4])
+        choices = input_checker("""
+        What would you like to investigate?(a/b/c/d) >>
+        """, OPTIONS[0:4])
 
         if choices == "c":
             typing(story.HOUSE_ITEM)
--- story.py
+++ story.py
@@ -1,43 +1,43 @@
 INTRO_MSG = ("""
- Your story begins with you waking up in the middle of a forest, 
+ Your story begins with you waking up in the middle of a forest,
  surrounded by the dark of the night with no memory of how you got there.
 
-All you do know, as reality starts to sink in, is that you have to 
+All you do know, as reality starts to sink in, is that you have to
 get out of there... alive.
-           
+
 Make the right choices, and you just might succeed.
 """)
 
 DEAD_GROVE = ("""
-            You walk towards the grove and stop at the entrace to look around, 
-            the fruits looks delicious.
-          
-            As you start walking to pick up some fruit for your journey,
-            you suddenly feel the ground beneath you give in..
-          
-            "AAAaaahh"....
-          
-            You wake up to find yourself  at the bottom of a pitfall. 
-            You look up and see a pair of glowing eyes with a huge grin 
-            looking down at you...
+You walk towards the grove and stop at the entrace to look around,
+the fruits looks delicious.
+
+As you start walking to pick up some fruit for your journey,
+you suddenly feel the ground beneath you give in..
+
+"AAAaaahh"....
+
+You wake up to find yourself  at the bottom of a pitfall.
+You look up and see a pair of glowing eyes with a huge grin
+looking down at you...
             """)
 
 FOUND_ITEM = ("""
-            You arrive at the source of the light you see a small leaf pile on 
-            the left side of the trail where the light is emanating from.
-          
-            You dig through the leafpile and find a flashlight! A strong one 
-            too, how strangely convenient!
+You arrive at the source of the light you see a small leaf pile on
+the left side of the trail where the light is emanating from.
+
+You dig through the leafpile and find a flashlight! A strong one
+too, how strangely convenient!
             """)
 
 ENCOUNTER = ("""
-            As you're crouched on the trail, proudly inspecting your newfound 
-            treasure, the hairs on your neck suddenly starts to rise..
+As you're crouched on the trail, proudly inspecting your newfound
+treasure, the hairs on your neck suddenly starts to rise..
 
-            You're being watched.
+You're being watched.
 
-            Some strange distorted noises emits from the direction you were 
-            walking towards.
+Some strange distorted noises emits from the direction you were
+walking towards.
             """)
 
 DEN_DEATH = ("""
@@ -93,53 +93,53 @@ As you reach the end of the tunnel you can see city lights in the distance..
 """)
 
 HOUSE = ("""
-           You start walking, continuing south off the path. 
+You start walking, continuing south off the path.
 
-           After a while you arrive at a small, abandoned house.  
-           You might find something useful in there, you go inside...
-     
-           Dusty with lots of spiderwebs, how cozy. Looks like no one has set 
-           foot in there for some time.
-          
-           Handwritten notes and books are scattered everywhere, someone's 
-           been busy.
+After a while you arrive at a small, abandoned house.  
+You might find something useful in there, you go inside...
 
-           You wonder what whomever was here was writing about, but it seems 
-           to be written in some language unkown to you.
-         
-           You look around...
-            """)
+Dusty with lots of spiderwebs, how cozy. Looks like no one has set
+foot in there for some time.
+
+Handwritten notes and books are scattered everywhere, someone's
+been busy.
+
+You wonder what whomever was here was writing about, but it seems
+to be written in some language unkown to you.
+
+You look around...
+""")
 
 HOUSE_ITEM = ("""
-           You direct your flashlight towards the floor underneath the desk, 
-           and you notice something silver sticking out from a pile of notes.
-           You put your flashlight down and crouch down to pick it up. 
-          
-           It looks like a very old flashlight in the shape of a square with a 
-           handle, you've never seen anything like it.
-          
-           There's some kind of switch on the back, you turn it and incredibly 
-           enough, it works!
+You direct your flashlight towards the floor underneath the desk,
+and you notice something silver sticking out from a pile of notes.
+You put your flashlight down and crouch down to pick it up.
 
-           The light it emits is weak though, and it feels very strange.. Warm 
-           and comforting somehow. 
+It looks like a very old flashlight in the shape of a square with a
+handle, you've never seen anything like it.
+
+There's some kind of switch on the back, you turn it and incredibly
+enough, it works!
+
+The light it emits is weak though, and it feels very strange.. Warm
+and comforting somehow.
            """)
 
 HOUSE_ENCOUNTER = ("""
-           You suddenly hear twigs cracking outside the broken window. 
-           Something is outside.
+You suddenly hear twigs cracking outside the broken window.
+Something is outside.
 
-           You take a deep breath and look toward the broken window. 
-           You see the shape of a distorted lower body, violently muscular and 
-           covered in rippling shadows.
+You take a deep breath and look toward the broken window.
+You see the shape of a distorted lower body, violently muscular and
+covered in rippling shadows.
 
-           You are frozen in horror, trying to decide what to do, when a huge 
-           clawed hand splinters the window frame.
-           Looks like it's about to crouch and take a look inside.
+You are frozen in horror, trying to decide what to do, when a huge
+clawed hand splinters the window frame.
+Looks like it's about to crouch and take a look inside.
            """)
 
 HOUSE_ENCOUNTER_OUTCOME = ("""
-The creature crouches slowly, and after what feels like forever, 
+The creature crouches slowly, and after what feels like forever,
 a pair of eyes and an abnormally large grin glowing in white appear
 in the window.
 
